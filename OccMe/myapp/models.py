47768e15bcbc6fb3d128a630c496dd774146a91c
abdoashraf56 <abdo.ashraf975@gmail.com>
2020-09-23 21:02:56 +0200

    migrate for first time

--- OccMe/myapp/models.py
+++ OccMe/myapp/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from .utilites import validatePhone
+from uuid import uuid4
 
 
 class Canadian (models.Model):
     """ Data Class contains Canadian information """
-    occupations = ["Electrics" , "Plumper" , "Mechanic" , "Carpenter" , "Other"]
-    
+
+    id = models.UUIDField(default=uuid4() , primary_key=True)
     firstname = models.CharField(max_length=70)
     lastname = models.CharField(max_length=70)
     email = models.EmailField()
     phone = models.CharField(max_length=11 , validators=[validatePhone])
-    image = models.ImageField(default="profile.png", null=True, blank=True)
-    occupation = models.CharField(max_length=70 , choices=occupations)
+    profile = models.ImageField(default="profile.png", null=True, blank=True)
+    occupation = models.CharField(max_length=70)
     rate = models.IntegerField()
-    latitude = models.DecimalField(8,6)
-    longitude = models.DecimalField(9,6)
+    latitude = models.DecimalField(max_digits=8, decimal_places=6 , null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places= 6 , null=True)
