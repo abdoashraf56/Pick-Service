bec8fd2d8a29edebdd44ee879de6e97548febe51
abdoashraf56 <abdo.ashraf975@gmail.com>
2020-09-29 14:02:41 +0200

    Add Occupation Model

--- OccMe/myapp/models.py
+++ OccMe/myapp/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from .validators import validatePhone
 from uuid import uuid4
 
+class Occupation (models.Model):
+    """ Data class contains Occupation name """
+    name = models.CharField(max_length=70)
+
+    def __str__(self) :
+        return f"{self.name}"
 
 class Canadian (models.Model):
     """ Data Class contains Canadian information """
@@ -18,7 +24,7 @@ class Canadian (models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=11 , validators=[validatePhone])
     profile = models.ImageField(default="profile.png", null=True, blank=True)
-    occupation = models.CharField(max_length=70 , choices=occupations)
+    occupation = models.ForeignKey(Occupation , null=True , related_name="canadians" , on_delete=models.SET_NULL)
     rate = models.IntegerField()
     latitude = models.DecimalField(max_digits=8, decimal_places=6 , null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places= 6 , null=True)
